6c1a050e4fc3258e11a45b6d384374a53c9880d8
Qohelet <benjy.shuter@gmail.com>
2023-08-04 04:30:56 -0400

    Added function to decide length

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+function getLength(){
+  var chosenLength = prompt("Choose number of characters in password (must be between 8 and 128 characters).");
+  var parsedInput = parseFloat(chosenLength)
+  if (isNaN(parsedInput)){
+    window.alert("ERROR: PASSWORD MUST BE A NUMBER.");
+    getLength();
+  } else if (!Number.isInteger(parsedInput)){
+    window.alert("ERROR: PASSWORD MUST BE A WHOLE NUMBER.");
+    getLength();
+  } else if (parsedInput<8){
+    window.alert("ERROR: PASSWORD MUST BE MORE THAN 8 CHARACTERS.");
+    getLength();
+  } else if (parsedInput>128){
+    window.alert("ERROR: PASSWORD MUST BE LESS THAN 128 CHARACTERS.");
+    getLength();
+  } else {
+    // return parsedInput
+    window.alert("Your password will be " + parsedInput + " characters long.");
+  };
+
+};
